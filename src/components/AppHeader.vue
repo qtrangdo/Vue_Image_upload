bf76179c4ee5ca7d4f8d70229be278a1c60e5032
td94 <quynhtrangdo94@gmail.com>
2019-02-14 23:03:48 -0800

    Persist login token using localStorage

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="ui secondary pointing menu">
     <a href="/" class="active item">Image Storage</a>
-    {{ isLoggedIn }}
     <div class="right menu">
-      <a href="#" class="ui item" @click="login">Login</a>
+      <div v-if="isLoggedIn">
+        Galleries button
+        Upload button
+        Logout button
+      </div>
+      <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
   </div>
 </template>
