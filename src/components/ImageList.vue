62725071a9bc517217648afde5a80e06c1bc7f18
td94 <quynhtrangdo94@gmail.com>
2019-02-16 02:02:57 -0800

    Fix bug with logout

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="image-container">
-    <img
-      v-for="image in allImages"
-      :src="image.link"
-      :key="image.id"
-    />
+  <div>
+    <div class="image-container" v-if="isLoggedIn">
+      <img v-for="image in allImages" :src="image.link" :key="image.id">
+    </div>
+    <h2 v-else>Log in to get started!</h2>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "ImageList",
-  computed: mapGetters(['allImages']),
-  methods: mapActions(['fetchImages']),
+  computed: mapGetters(["allImages", "isLoggedIn"]),
+  methods: mapActions(["fetchImages"]),
   created() {
     this.fetchImages();
   }
-}
+};
 </script>
 
 <style scoped>
-.image-container{
+.image-container {
   column-count: 3;
   column-gap: 0;
 }
