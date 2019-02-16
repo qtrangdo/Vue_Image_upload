98c5ca88f40a400ca99bdfdaaaa106980174381e
td94 <quynhtrangdo94@gmail.com>
2019-02-16 00:51:11 -0800

    Style upload form

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>
-    Upload Form
+  <div class="dropper">
+    <input type="file">
+    <span>Drag files here!</span>
   </div>
 </template>
 
 <script>
 export default {
   name: "UploadForm"
-}
+};
 </script>
+
+<style scoped>
+.dropper {
+  height: 30vh;
+  border: 2px dashed black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.dropper:hover {
+  cursor: pointer;
+  background-color: rgb(207, 248, 235);
+}
+
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute ;
+  opacity: 0;
+}
+</style>
